b7d8e93ce9f29480c957177799f3660b2dd50309
Monika0808 <mnoi22082003@gamil.com>
2024-03-09 18:26:22 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FG%2F31%2Famazonservices%2FBlog%2FeCommerce_Platform.png&tbnid=OeU0cZFUfkXjhM&vet=12ahUKEwiw6sbyh-eEAxW_1DgGHWVTC_UQMygjegUIARC0AQ..i&imgrefurl=https%3A%2F%2Fsell.amazon.in%2Fseller-blog%2Fwhat-is-an-ecommerce-platform-and-how-can-you-get-benefit-out-of-it&docid=NPVP8ToZuQP5HM&w=1800&h=1000&q=e%20commerce%20platform&safe=active&ved=2ahUKEwiw6sbyh-eEAxW_1DgGHWVTC_UQMygjegUIARC0AQ" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,2 @@
+
+export const API = "https://backend-2v6l.vercel.app"
